b7a387eee40517a47d1598d3a5977280c881d1a0
Leo <lyjberserker@gmail.com>
2013-05-25 13:45:48 +0800

    leetcode

---
+++ code/src/question/ConstructBinaryTreeInorderPostorderTraversal.java
@@ -0,0 +1,33 @@
+package question;
+
+public class ConstructBinaryTreeInorderPostorderTraversal {
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		return buildTree(preorder, 0, preorder.length - 1, inorder, 0,
+				inorder.length - 1);
+	}
+
+	public TreeNode buildTree(int[] preorder, int pb, int pe, int[] inorder,
+			int ib, int ie) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		if (pb > pe || ib > ie)
+			return null;
+		TreeNode t = new TreeNode(preorder[pb]);
+		int index = findIndex(inorder, preorder[pb]);
+		t.left = buildTree(preorder, pb + 1, pb + index - ib, inorder, ib,
+				index - 1);
+		t.right = buildTree(preorder, pb + index - ib + 1, pe, inorder,
+				index + 1, ie);
+		return t;
+	}
+
+	public int findIndex(int[] num, int x) {
+		for (int i = 0; i < num.length; i++) {
+			if (num[i] == x)
+				return i;
+		}
+		return -1;
+	}
+}
---
+++ code/src/question/ConstructBinaryTreePreorderInorderTraversal.java
@@ -0,0 +1,33 @@
+package question;
+
+public class ConstructBinaryTreePreorderInorderTraversal {
+	public TreeNode buildTree(int[] inorder, int[] postorder) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		return buildTree(inorder, 0, inorder.length - 1, postorder, 0,
+				postorder.length - 1);
+	}
+
+	public TreeNode buildTree(int[] inorder, int ib, int ie, int[] postorder,
+			int pb, int pe) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		if (ib > ie || pb > pe)
+			return null;
+		TreeNode t = new TreeNode(postorder[pe]);
+		int index = findIndex(inorder, postorder[pe]);
+		t.left = buildTree(inorder, ib, index - 1, postorder, pb, pb + index
+				- ib - 1);
+		t.right = buildTree(inorder, index + 1, ie, postorder, pb + index - ib,
+				pe - 1);
+		return t;
+	}
+
+	public int findIndex(int[] num, int x) {
+		for (int i = 0; i < num.length; i++) {
+			if (num[i] == x)
+				return i;
+		}
+		return -1;
+	}
+}
