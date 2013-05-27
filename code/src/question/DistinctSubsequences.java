f5a95c2ded1a8ea1132b655f254348112182f332
Leo <lyjberserker@gmail.com>
2013-05-27 17:13:14 +0800

    leetcode

---
+++ code/src/question/DistinctSubsequences.java
@@ -0,0 +1,17 @@
+package question;
+
+public class DistinctSubsequences {
+	public int numDistinct(String S, String T) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		int[] dp = new int[T.length() + 1];
+		dp[0] = 1;
+		for (int i = 0; i < S.length(); i++) {
+			for (int j = T.length() - 1; j >= 0; j--) {
+				if (S.charAt(i) == T.charAt(j))
+					dp[j + 1] += dp[j];
+			}
+		}
+		return dp[T.length()];
+	}
+}
