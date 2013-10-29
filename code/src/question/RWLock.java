872a04efbc50278f35485eb5e73ea27582f7a5ca
Leo <lyjberserker@gmail.com>
2013-10-29 17:39:24 +0800

    leetcode

--- code/src/question/RWLock.java
+++ code/src/question/RWLock.java
@@ -1,5 +1,33 @@
 package question;
 
 public class RWLock {
+	private int readers = 0;
 
+	private int writers = 0;
+
+	private int writeRequest = 0;
+
+	public synchronized void lockRead() throws InterruptedException {
+		while (writers > 0 || writeRequest > 0)
+			wait();
+		readers++;
+	}
+
+	public synchronized void unlockRead() {
+		readers--;
+		notifyAll();
+	}
+
+	public synchronized void lockWrite() throws InterruptedException {
+		writeRequest++;
+		while (readers > 0 || writers > 0)
+			wait();
+		writeRequest--;
+		writers++;
+	}
+
+	public synchronized void unlockWrite() {
+		writers--;
+		notifyAll();
+	}
 }
